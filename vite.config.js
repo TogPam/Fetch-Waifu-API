2a87356e7bfffb598c135cb5cd2e8b3208ec3fff
TogPam <rthong.pam@gmail.com>
2025-01-08 15:10:06 +0700

    u

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Fetch-Waifu-API'
+  base: '/Fetch-Waifu-API/'
 })

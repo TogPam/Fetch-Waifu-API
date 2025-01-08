f894c95e65421ff9060ddc0b67ffcb7b459390e8
TogPam <rthong.pam@gmail.com>
2025-01-08 15:01:08 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Fetch-Waifu-API',
-  server: {
-    port: '6969'
-  }
+  base: '/Fetch-Waifu-API'
 })
